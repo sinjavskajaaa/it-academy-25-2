c14316d744442da8bbdf4267eebf0115fea6830f
Alina Sinjavskaja <sinjavskajaaa@gmail.com>
2025-10-19 14:11:44 +0300

    Changes in task3

--- lesson-3/task.js
+++ lesson-3/task.js
@@ -1,27 +1,28 @@
 const fs = require('fs-extra');
+const path = require('path');
 
 //Создать папку
 fs.mkdirSync('Folder-1');
 
 //Создать текстовый файл в созданной папке
-fs.createFileSync('lesson-3/Folder-1/file-1.txt');
+fs.createFileSync('Folder-1/file.txt');
 
 //создать вторую папку
 fs.mkdirSync('Folder-2');
 
 //Переместить файл из первой папки во вторую
-fs.moveSync('/lesson-3/Folder-1/file-1.txt', '/lesson-3/Folder-2', { overwrite: true })
+fs.moveSync('/Folder-1/file.txt', '/Folder-2', { overwrite: true })
 
 //Создание третью папку
 fs.mkdirSync('Folder-3');
 
 //скопировать файл из второй в третью папку
-fs.moveSync('/lesson-3/Folder-2/file-1.txt', '/lesson-3/Folder-3', { overwrite: true })
+fs.moveSync('/Folder-2/file.txt', '/Folder-3', { overwrite: true })
 
 //Удалить файлы
-fs.removeSync('/lesson-3/Folder-3/file-1.txt')
+fs.removeSync('/Folder-3/file.txt')
 
 //удалить все папки
-fs.removeSync('./lesson-3/Folder-1');
-fs.removeSync('./lesson-3/Folder-2');
-fs.removeSync('./lesson-3/Folder-3');
+fs.removeSync('./Folder-1');
+fs.removeSync('./Folder-2');
+fs.removeSync('./Folder-3');
