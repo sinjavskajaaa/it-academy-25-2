5f7dafded58c9599b8ef6954039bbaf4b3fec144
Alina Sinjavskaja <sinjavskajaaa@gmail.com>
2025-10-22 18:19:57 +0300

    Task3

--- lesson-3/task3.js
+++ lesson-3/task3.js
@@ -1,12 +1,12 @@
 const fs = require('fs-extra');
 
-    //Создать папку
+//Создать папку
     fs.mkdirpSync('./Folder-1');
 
 //Создать текстовый файл в созданной папке
     fs.createFileSync('./Folder-1/file.txt');
 
-//создать вторую папку
+//Cоздать вторую папку
     fs.mkdirpSync('./Folder-2');
 
 //Переместить файл из первой папки во вторую
@@ -15,13 +15,13 @@ const fs = require('fs-extra');
 //Создание третью папку
     fs.mkdirpSync('./Folder-3');
 
-//скопировать файл из второй в третью папку
+//Cкопировать файл из второй в третью папку
     fs.moveSync('./Folder-2/file.txt', './Folder-3/file.txt', { move: true })
 
 //Удалить файлы
     fs.removeSync('./Folder-3/file.txt')
 
-//удалить все папки
+//Yдалить все папки
     fs.removeSync('./Folder-1');
     fs.removeSync('./Folder-2');
     fs.removeSync('./Folder-3');
